7c61bbb3dc2ced0f053709b128e9eab48ff52920
VaishaliDixit <vaishalidixit@cargill.com>
2021-10-30 12:38:40 +0530

    feat(cards): added ui for job cards

--- src/pages/explore_jobs/JobCards.js
+++ src/pages/explore_jobs/JobCards.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function JobCards() {
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+
+function JobCards({ data }) {
+    const [display, setDisplay] = useState(false);
+
     return (
-        <div>
-            
+        <div className="p-d-flex p-flex-wrap p-jc-evenly">
+            {data.map((d, i) =>
+                <>
+                    <div className="p-d-flex p-col-4 job-card p-m-3" key={i}>
+                        <div className="p-col-4">
+                            <img alt={"image" + i} className="card-img"
+                                src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2940&q=80" />
+                        </div>
+                        <div className="p-d-flex p-col-8 p-flex-wrap p-text-left">
+                            <div className="p-col-12">{d.name}</div>
+                            <div className="p-col-12">
+                                Location: {d.locations.map(l => l.name).join(', ').slice(0, 12) + '...'}
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            Comapny: {d.company.name}
+                            </div>
+                            <div className="p-col-12">{d.contents.slice(0, 100) + '...'}</div>
+                            <div className="p-col-12 p-text-right">
+                                <Button label="View Details" onClick={() => setDisplay(true)}></Button>
+                            </div>
+                        </div>
+                    </div>
+                    <Dialog header={d.name} visible={display} style={{ width: '50vw' }}>
+                        <p>Job ID: {d.id}</p>
+                        <p>Description: {d.contents}</p>
+                    </Dialog>
+                </>
+            )}
         </div>
     )
 }
--- src/pages/explore_jobs/index.js
+++ src/pages/explore_jobs/index.js
@@ -1,7 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import JobCards from './JobCards';
+
+import '../../styles/Explore.css';
 
 function ExploreJobs() {
     const [data, setData] = useState([]);
+
+    const getJobsList = () => {
+        axios.get('https://www.themuse.com/api/public/jobs?page=1')
+            .then((res) => {
+                setData(res.data.results);
+            })
+            .catch(err => console.log(err));
+    }
 
     useEffect(() => {
         getJobsList();
@@ -9,8 +22,7 @@ function ExploreJobs() {
 
     return (
         <div>
-            here
-            <JobCards />
+            <JobCards data={data} />
         </div>
     )
 }
